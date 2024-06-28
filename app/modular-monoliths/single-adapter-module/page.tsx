f1e6c6068ad15015b1644f859bbaad311b85661b
Alex Basson <abasson@vmware.com>
2024-06-28 16:58:27 -0400

    Draw diagram for single adapter module

--- app/modular-monoliths/single-adapter-module/page.tsx
+++ app/modular-monoliths/single-adapter-module/page.tsx
@@ -1,7 +1,92 @@
+'use client'
+
+import useFrame from "@/app/lib/useFrame";
+import {adapterRed, deployableGreen, Geometry, layoutPadding, policyBlue, Port} from "@/app/lib/definitions";
+import systemGeometry from "@/app/lib/diagrams/systemGeometry";
+import DomainModule from "@/app/lib/diagrams/DomainModule";
+import PrimaryAdapter from "@/app/lib/diagrams/PrimaryAdapter";
+import SecondaryAdapter from "@/app/lib/diagrams/SecondaryAdapter";
+import DeployableModule from "@/app/lib/diagrams/DeployableModule";
+import PrimaryPort from "@/app/lib/diagrams/PrimaryPort";
+import SecondaryPort from "@/app/lib/diagrams/SecondaryPort";
+
+type Frame = {
+
+}
+
 export default function Page() {
+  const frames: Frame[] = [
+    {
+    },
+  ];
+
+  const frame = useFrame<Frame>(frames);
+
+  const height = window.innerHeight - 2*layoutPadding;
+  const width = window.innerWidth - 2*layoutPadding;
+
+  const scale = height / 4;
+  const policyAGeometry = systemGeometry(scale, 0.3 * width, 0.5 * height);
+  const policyBGeometry = systemGeometry(scale, 0.7 * width, 0.5 * height);
+  const deployableGeometry = systemGeometry(scale, width/2, 0.5 * height);
+
+  const transform = (geometry: Geometry, port: Port) =>  `
+  translate(${port.center.x + port.adapterOffset.x + geometry.center.x}, ${port.center.y + port.adapterOffset.y + geometry.center.y}),
+  rotate(${port.rotate}, 0, 0)
+  `
+
+  const points = `
+  ${policyAGeometry.center.x + scale}, ${policyAGeometry.center.y},
+  ${policyAGeometry.center.x + scale / 2}, ${policyAGeometry.center.y - (Math.sqrt(3) / 2) * scale},
+  ${policyBGeometry.center.x - scale / 2}, ${policyBGeometry.center.y - (Math.sqrt(3) / 2) * scale},
+  ${policyBGeometry.center.x - scale}, ${policyBGeometry.center.y},
+  `
+
   return (
     <div>
-      <h1>single adapter module</h1>
+      <svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+        <DomainModule geometry={policyAGeometry} fill={policyBlue.hexValue} text="organizing games"/>
+        <PrimaryAdapter geometry={policyAGeometry} portName={'nwPort'}/>
+        <PrimaryAdapter geometry={policyAGeometry} portName={'swPort'}/>
+        <SecondaryAdapter geometry={policyAGeometry} portName={'sePort'}/>
+
+        <DomainModule geometry={policyBGeometry} fill={policyBlue.hexValue} text="gameplay"/>
+        <PrimaryAdapter geometry={policyBGeometry} portName={'swPort'}/>
+        <SecondaryAdapter geometry={policyBGeometry} portName={'nePort'}/>
+        <SecondaryAdapter geometry={policyBGeometry} portName={'sePort'}/>
+
+        {/* cross policy adapter */}
+
+        <g>
+          <g transform={`${transform(policyAGeometry, policyAGeometry.nePort)}`}>
+            <SecondaryPort
+              sideLength={0.8 * scale}
+              fill={adapterRed.hexValue}
+            />
+          </g>
+
+          <g transform={`${transform(policyBGeometry, policyBGeometry.nwPort)}`}>
+            <PrimaryPort
+              sideLength={0.8 * scale}
+              fill={adapterRed.hexValue}
+            />
+          </g>
+
+          <polygon points={points} fill={adapterRed.hexValue} />
+
+          <text
+            x={(policyAGeometry.center.x + policyBGeometry.center.x) / 2}
+            y={(policyAGeometry.center.y - (Math.sqrt(3) / 4) * scale)}
+            textAnchor='middle'
+            dominantBaseline='middle'
+          >
+            adapter
+          </text>
+        </g>
+
+        <DeployableModule geometry={deployableGeometry} stroke={deployableGreen.hexValue} width={0.47 * width}
+                          height={1.7 * scale}/>
+      </svg>
     </div>
   )
 }
