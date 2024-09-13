2bbed31452e3d2b2839632ea03f3061f910d3d2b
Alex Basson <abasson@vmware.com>
2024-09-13 11:47:14 -0400

    Thank Cedar for allowing me to give the talk

--- app/modular-monoliths/thank-you/page.tsx
+++ app/modular-monoliths/thank-you/page.tsx
@@ -3,7 +3,8 @@
 export default function Page() {
   return (
     <div className='padding-horizontal'>
-      <p className={'text-xl text-center py-[100px]'}>Thank you</p>
+      <p className={'text-xl py-[100px]'}>Thank you</p>
+      <p>(and thanks again to Cedar!)</p>
     </div>
   )
 }
