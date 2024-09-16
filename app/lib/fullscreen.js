e57fa0a35ada296922a9706be7984d94f8413baa
Alex Basson <abasson@vmware.com>
2024-09-16 09:56:22 -0400

    Allow for fullscreen presetation

---
+++ app/lib/fullscreen.js
@@ -0,0 +1,19 @@
+export const enterFullscreen = (elem) => {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  }
+}
+
+export const exitFullscreen = () => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
--- app/modular-monoliths/page.tsx
+++ app/modular-monoliths/page.tsx
@@ -1,5 +1,25 @@
+'use client'
+
+import {useRef} from "react";
+import {enterFullscreen, exitFullscreen} from "@/app/lib/fullscreen";
+
 export default function Page() {
+
+  const presentationRef = useRef(null);
+
+  const handleFullscreen = () => {
+    if (document.fullscreenElement) {
+      exitFullscreen();
+    } else {
+      enterFullscreen(presentationRef.current);
+    }
+  }
+
   return (
-    <p>Start</p>
+    <div ref={presentationRef} className={"#app"}>
+      <button onClick={handleFullscreen} className={"fullscreen"}>Present</button>
+
+      <p>Start</p>
+    </div>
   )
 }
