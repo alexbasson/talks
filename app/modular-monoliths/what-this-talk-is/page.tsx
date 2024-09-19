7a7f49c61b3b1e40160bfc3734e5f5de686caf67
Alex Basson <abasson@vmware.com>
2024-09-19 14:44:43 -0400

    Add frames to 'what this talk is' slide

--- app/modular-monoliths/what-this-talk-is/page.tsx
+++ app/modular-monoliths/what-this-talk-is/page.tsx
@@ -4,15 +4,31 @@ import useFrame from "@/app/lib/useFrame"
 
 type Frame = {
   displayMath: boolean,
+  displayCostsVBenefits: boolean,
+  displayQuestion: boolean,
 }
 
 export default function Page() {
   const frames: Frame[] = [
     {
       displayMath: false,
+      displayCostsVBenefits: false,
+      displayQuestion: false,
     },
     {
       displayMath: true,
+      displayCostsVBenefits: false,
+      displayQuestion: false,
+    },
+    {
+      displayMath: true,
+      displayCostsVBenefits: true,
+      displayQuestion: false,
+    },
+    {
+      displayMath: true,
+      displayCostsVBenefits: true,
+      displayQuestion: true,
     },
   ]
 
@@ -22,6 +38,8 @@ export default function Page() {
     <div className='padding-horizontal'>
       <p>what this talk is</p>
       { frame.displayMath ? <p className={'text-xl text-yellow-600 font-mono'}>math</p> : <></> }
+      { frame.displayCostsVBenefits ? <p>weighing costs versus benefits</p> : <></> }
+      { frame.displayQuestion ? <p>how can we get more benefits for less cost?</p> : <></> }
     </div>
   )
 }
