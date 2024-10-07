1962aace93862e81fdd5c4a78dcd9c86d730da8e
Alex Basson <abasson@vmware.com>
2024-10-07 16:31:35 -0400

    Add bullet point animation in module definition slides

--- app/modular-monoliths/bounded-contexts/page.tsx
+++ app/modular-monoliths/bounded-contexts/page.tsx
@@ -28,6 +28,7 @@ export default function Page() {
   return (
     <div className='padding-horizontal'>
       <h1>bounded contexts</h1>
+      <p className='text-sm'>Eric Evans</p>
       { frame.displayDefinition ? <p className={"mt-16 leading-tight"}>&ldquo;the scope of a particular model...within which a single<br/>model will apply and will be kept as unified as possible&rdquo;</p> : <></> }
       { frame.displayModel ? <p className={"mt-16 leading-tight"}>&ldquo;the team&apos;s agreed-upon way of structuring domain<br/>knowledge and distinguishing the elements of most interest&rdquo;</p> : <></> }
     </div>
--- app/modular-monoliths/deployable-modules/page.tsx
+++ app/modular-monoliths/deployable-modules/page.tsx
@@ -1,18 +1,60 @@
+'use client'
+
 import {deployableGreen} from "@/app/lib/definitions";
+import useFrame from "@/app/lib/useFrame";
+
+type Frame = {
+  bullet1: boolean,
+  bullet2: boolean,
+  bullet3: boolean,
+}
 
 export default function Page() {
+  const frames: Frame[] = [
+    {
+      bullet1: false,
+      bullet2: false,
+      bullet3: false,
+    },
+    {
+      bullet1: true,
+      bullet2: false,
+      bullet3: false,
+    },
+    {
+      bullet1: true,
+      bullet2: true,
+      bullet3: false,
+    },
+    {
+      bullet1: true,
+      bullet2: true,
+      bullet3: true,
+    },
+  ]
+
+  const frame = useFrame<Frame>(frames);
+
   return (
     <div className='padding-horizontal' style={{display: 'grid', gridTemplateColumns: '2fr 1fr'}}>
       <div>
         <h1 className={deployableGreen.className}>deployable modules</h1>
         <ul className={"pl-16 list-disc"}>
-          <li>1 deployable module = 1 deployable service</li>
-          <li><span className={deployableGreen.className}>imports everything to be deployed</span> in this service</li>
-          <li>
-            defines <span className={deployableGreen.className}>configuration</span>,<br/>
-            knows about the deployment <span className={deployableGreen.className}>environment</span><br/>
-            contains <span className={deployableGreen.className}>database migrations</span> for the service&apos;s db
-          </li>
+          { frame.bullet1
+            ? <li>1 deployable module = 1 deployable service</li>
+            : <></>
+          }
+          { frame.bullet2
+            ? <li><span className={deployableGreen.className}>imports everything to be deployed</span> in this service</li>
+            : <></>
+          }
+          { frame.bullet3 ?
+            <li>
+              defines <span className={deployableGreen.className}>configuration</span>,<br/>
+              knows about the deployment <span className={deployableGreen.className}>environment</span><br/>
+              contains <span className={deployableGreen.className}>database migrations</span> for the service&apos;s db
+            </li>
+            : <></> }
         </ul>
       </div>
     </div>
