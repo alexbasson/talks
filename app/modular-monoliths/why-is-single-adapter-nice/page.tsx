1de232bd28bc33a2b1a65b069b5b83aa964628df
Alex Basson <alex.basson@primer.ai>
2025-08-16 10:30:14 -0400

    More idiomatic python tweaks

--- app/modular-monoliths/why-is-single-adapter-nice/page.tsx
+++ app/modular-monoliths/why-is-single-adapter-nice/page.tsx
@@ -2,7 +2,7 @@
 
 import useFrame from "@/app/lib/useFrame";
 
-import {adapterRed, deployableGreen, policyBlue} from "@/app/lib/colors";
+import {policyBlue} from "@/app/lib/colors";
 
 type Frame = {
   bullet1: boolean,
@@ -76,8 +76,8 @@ export default function Page() {
   const frame = useFrame<Frame>(frames);
 
   return (
-    <div className='padding-horizontal grid grid-cols-2 gap-8'>
-      <div>
+    <div className='padding-horizontal w-full flex justify-start'>
+      <div className='mr-8'>
         <h1>why is this nice?</h1>
         <ul className='pl-16 list-disc'>
           {frame.bullet1 ? <li>each piece <span className={"italic"}>actually</span> stays small</li> : <></>}
@@ -88,39 +88,34 @@ export default function Page() {
       </div>
       {frame.bullet2 ?
         <div className='font-mono text-sm leading-normal mt-4'>
-          <p className='mb-4'>online-chess/</p>
-          <ul className='pl-16 list-none'>
-            <li className={`${policyBlue.className} p-2`}>
-              <p>organizing-games-policy/player.py</p>
-            </li>
-            { frame.orgGamesPlayer ?
-              <li className='ml-16'>
-                <p>
-                  class Player():<br/>
-                  &nbsp;&nbsp;__init__(username, email):<br/>
-                  &nbsp;&nbsp;&nbsp;&nbsp;self.username = username<br/>
-                  &nbsp;&nbsp;&nbsp;&nbsp;self.email = email<br/>
-                </p>
-              </li>
-              : <></>
-            }
-            <li className={`${policyBlue.className} p-2 mt-16`}>
-              <p>gameplay-policy/player.py</p>
-            </li>
-            { frame.gamePlayPlayer ?
-              <li className='ml-16'>
-                <p>
-                  class Player():<br/>
-                  &nbsp;&nbsp;__init__(color):<br/>
-                  &nbsp;&nbsp;&nbsp;&nbsp;self.color = color<br/>
-                </p>
-              </li>
-              : <></>
-            }
-          </ul>
+          <p className='mb-8'>online-chess/</p>
+
+          <div className='pl-16'>
+            <div className='mb-8'>
+              <p className={`${policyBlue.className} p-2`}>organizing-games-policy/player.py</p>
+              { frame.orgGamesPlayer ? <PlayerClass params={['username', 'email']} /> : <></> }
+            </div>
+
+            <div>
+              <p className={`${policyBlue.className} p-2`}>gameplay-policy/player.py</p>
+              { frame.gamePlayPlayer ? <PlayerClass params={['color']}/> : <></> }
+            </div>
+          </div>
+
         </div>
         : <></>
       }
     </div>
+  )
+}
+
+function PlayerClass({ params }: { params: string[] }) {
+  return (
+    <p className='ml-16'>
+      class Player:<br/>
+      &nbsp;&nbsp;def __init__(self, {params.join(', ')}):<br/>
+      { params.map(param => <>&nbsp;&nbsp;&nbsp;&nbsp;self.{param} = {param}<br/></>) }
+      <br/>
+    </p>
   )
 }
